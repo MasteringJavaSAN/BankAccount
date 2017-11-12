f164cd5dc05f65f359d59d16c9f845dad2a067ca
Sanjay <sanjaykv7@gmail.com>
2017-11-12 11:37:50 +0530

    Initial commit

---
+++ src/com/sanjay/BankDetails.java
@@ -0,0 +1,70 @@
+package com.sanjay;
+
+public class BankDetails {
+    private int accountNumber;
+    private int balance;
+    private String customerName;
+    private String customerEmail;
+    private String phoneNumber;
+
+    /*
+    Getters and setters for Account Number
+    */
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public int getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    /*
+    Getters and setters for Balance
+    */
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public int getBalance() {
+        return this.balance;
+    }
+
+    /*
+    Getters and setters for Customer Name
+    */
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerName() {
+        return this.customerName;
+    }
+
+    /*
+    Getters and setters for Email ID
+    */
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getCustomerEmail() {
+        return this.customerEmail;
+    }
+
+    /*
+    Getters and setters for Phone Number
+    */
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+   /*
+   Done with Getters and Setters *************************************************************************************
+   */
+
+
+}
